9409842711f7f417ee80f403a3b30bcce3e596e0
James Tran <99104708+jamestran12@users.noreply.github.com>
2026-05-21 18:01:04 +1000

    Update AutomaticQuantisation.Designer.cs

--- AutomaticQuantisation.Designer.cs
+++ AutomaticQuantisation.Designer.cs
@@ -1,16 +1,54 @@
-using System.Drawing.Text;
-using System.Runtime.CompilerServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace reaper_auto_quantisation
 {
-    public partial class AutomaticQuantisation : Form
+    partial class AutomaticQuantisation
     {
-        public AutomaticQuantisation()
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
         {
-            InitializeComponent();
-            Menu m = new Menu();
-            m.Dock = DockStyle.Fill;
-            this.Controls.Add(m);
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutomaticQuantisation));
+            SuspendLayout();
+            // 
+            // AutomaticQuantisation
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(782, 553);
+            Cursor = Cursors.IBeam;
+            Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MaximumSize = new Size(800, 600);
+            Name = "AutomaticQuantisation";
+            Text = "Audio Quantisation Automator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
     }
 }
